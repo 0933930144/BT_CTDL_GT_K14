8e41c363026ba9cd1843f0757f7850f84cb3b73c
Duongxuankhai <duongxuankhai@gmail.com>
2021-10-25 07:22:55 +0700

    1

--- src/com/khaidx2008110188/tuan7/LinkedList.java
+++ src/com/khaidx2008110188/tuan7/LinkedList.java
@@ -1,4 +1,5 @@
 package com.khaidx2008110188.tuan7;
+
 public class LinkedList {
     Node head = null;
     Node tail = null;
@@ -6,106 +7,114 @@ public class LinkedList {
     // int data;
     // Node n = new Node(data);
 
-    public LinkedList(){}
+    public LinkedList() {
+    }
 
-    void addTail(int data){
+    void addTail(int data) {
         Node newNode = new Node(data);
 
-        if(head == null){
+        if (head == null) {
             head = newNode;
             tail = newNode;
-        }else{
+        } else {
             tail.next = newNode;
             tail = newNode;
         }
     }
-    void addHead(int data){
+
+    void addHead(int data) {
         Node newNode = new Node(data);
 
-        if(head == null){
+        if (head == null) {
             head = newNode;
             tail = newNode;
-        }else{
+        } else {
             newNode.next = head;
             head = newNode;
         }
     }
-    void inDanhSach(){
+
+    void inDanhSach() {
         Node current = head;
 
-        if(head == null){
+        if (head == null) {
             System.out.println("Danh Sach Rong");
-        }else{
+        } else {
             System.out.println("Danh Sach Cac Bien: ");
-            while(current != null){
+            while (current != null) {
                 System.out.println(current.data);
                 current = current.next;
             }
         }
     }
-    void add(int data, int n){
+
+    void add(int data, int n) {
         Node current = head;
-        if(head.data == n){
+        if (head.data == n) {
             addHead(data);
             return;
         }
-        while(current.next != null){
-            if(current.next.data == n){
+        while (current.next != null) {
+            if (current.next.data == n) {
                 Node t = new Node(data);
                 t.next = current.next;
                 current.next = t;
                 return;
             }
             current = current.next;
-        }   
+        }
     }
-    void removeHead(){
-        Node tam = head;
-        if(head == null){
+
+    void removeHead() {
+        Node tempNode = head;
+        if (head == null) {
             System.out.println("Danh Sach Rong");
             return;
         }
-            head = tam.next;
+        head = temp.next;
     }
-    void removeTail(){
-        Node tam = head;
-        if(head == null){
+
+    void removeTail() {
+        Node temp = head;
+        if (head == null) {
             System.out.println("Danh Sach Rong");
             return;
         }
-        while(tam != null){
-            if(tam.next == tail){
-                tail = tam;
+        while (tam != null) {
+            if (temp.next == tail) {
+                tail = temp;
                 tail.next = null;
                 return;
             }
-            tam = tam.next;
+            temp = temp.next;
         }
     }
-    void remove(int n){
-        Node tam1 = head;
-        Node tam2 = null;
-        if(tam1 != null && tam1.data == n){
+
+    void remove(int n) {
+        Node temp1 = head;
+        Node temp2 = null;
+        if (temp1 != null && temp1.data == n) {
             removeHead();
             return;
         }
-        if(tam1 != null && tail.data == n){
+        if (temp1 != null && tail.data == n) {
             removeTail();
             return;
         }
-        while(tam1 != null){
-            tam2 = tam1;
-            tam1 = tam1.next;
-            if(tam1.data == n){
-                tam2.next = tam1.next;
+        while (temp1 != null) {
+            temp2 = temp1;
+            temp1 = temp1.next;
+            if (temp1.data == n) {
+                temp2.next = temp1.next;
                 return;
             }
         }
     }
-    void edit(int n, int t){
+
+    void edit(int n, int t) {
         Node current = head;
-        while(current != null){
-            if(current.data == n)
+        while (current != null) {
+            if (current.data == n)
                 current.data = t;
             current = current.next;
         }
--- src/com/khaidx2008110188/tuan7/Node.java
+++ src/com/khaidx2008110188/tuan7/Node.java
@@ -1,16 +1,18 @@
 package com.khaidx2008110188.tuan7;
-public class Node{
+
+public class Node {
     int data;
     Node next;
 
-    public Node(){}
-    
-    public Node(int d){
+    public Node() {
+    }
+
+    public Node(int d) {
         data = d;
         next = null;
     }
 
-    public Node(int d, Node n){
+    public Node(int d, Node n) {
         data = d;
         next = n;
     }
